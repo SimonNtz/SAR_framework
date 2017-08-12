e0f2e1f5453f11558da03241487c66bdc98ad160
SimonNtz <simon.ntz@gmail.com>
2017-08-12 13:52:45 +0200

    refactoring on github

--- deployment.sh
+++ deployment.sh
@@ -1,6 +1,27 @@
-#!/bin/bash
+  #!/bin/bash
 set -e
 set -x
 
-echo "`ss-get hostname`    $(hostname)" >> /etc/hosts
-ss-set machine-hn `hostname`
+
+_ss_set_hostname() {
+  echo "`ss-get hostname`    $(hostname)" >> /etc/hosts
+  ss-set machine-hn `hostname`
+}
+
+_instal_server_module() {
+  pip install Flask
+  pip install boto
+  pip install requests
+}
+
+_install_python_APIs() {
+  pip install slipstream-api
+  pip install elasticsearch
+}
+
+ _ss_set_hostname
+ _install_python_APIs
+_instal_server_module
+
+cd ~/SAR_framework/
+python server3.py
