50cda84dd997633cc554703cbeca605bc80baa0a
SimonNtz <simon.ntz@gmail.com>
2017-08-15 09:22:42 +0200

    cesnet is back

--- deployment/dmm/server_dmm.py
+++ deployment/dmm/server_dmm.py
@@ -265,14 +265,14 @@ def _components_service_offers(cloud, specs):
 
 def deploy_run(cloud, product, serviceOffers, offer, time ):
 
-    # mapper_so = "service-offer/cc382a2d-20f4-499d-82c2-046873e0cd05"
-    # reducer_so = "service-offer/cc382a2d-20f4-499d-82c2-046873e0cd05"
-    # cloud = "eo-cesnet-cz1"
+    mapper_so = "service-offer/cc382a2d-20f4-499d-82c2-046873e0cd05"
+    reducer_so = "service-offer/cc382a2d-20f4-499d-82c2-046873e0cd05"
+    cloud = "eo-cesnet-cz1"
 
     server_ip   = sys.argv[3]
     server_hostname   = sys.argv[4]
-    mapper_so =  serviceOffers['mapper']
-    reducer_so =  serviceOffers['reducer']
+    # mapper_so =  serviceOffers['mapper']
+    # reducer_so =  serviceOffers['reducer']
 
     deploy_id = "Not deployed"
     if mapper_so and reducer_so:
